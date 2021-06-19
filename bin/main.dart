7b4b914f76d39cbe4cacc49ac60fd7a43b476453
desacu <noemidsc@gmail.com>
2021-06-19 12:01:10 +0100

    REFACTOR - returns the sum of the numbers it receives separated by commas

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,15 @@
 int stringCalculator(String stringNumbers) {
   if (stringNumbers.isEmpty) return 0;
-  if (stringNumbers == '1,2') return 3;
-  if (stringNumbers == '1,2,3') return 6;
+  if (stringNumbers.contains(',')) return _sumNumbersSeparatedByComma(stringNumbers);
   return stringNumbers.toInt();
+}
+
+int _sumNumbersSeparatedByComma(String numbers) {
+  var total = 0;
+
+  numbers.split(',').forEach((number) => total += number.toInt());
+
+  return total;
 }
 
 extension StringExtencion on String {
