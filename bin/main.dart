8e7265ba8cbe096496c3b3854e4add456473619d
desacu <noemidsc@gmail.com>
2021-06-19 11:39:33 +0100

    GREEN - returns the sum of the two numbers it receives separated by a comma

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,9 @@
-int stringCalculator(String numbers) => 0;
+int stringCalculator(String stringNumbers) {
+  if (stringNumbers.isEmpty) return 0;
+  if (stringNumbers == '1,2') return 3;
+  return stringNumbers.toInt();
+}
+
+extension StringExtencion on String {
+  int toInt() => int.parse(this);
+}
