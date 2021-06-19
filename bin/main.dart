2c22247e974d2d29be87b4be576bcac1a72277a4
desacu <noemidsc@gmail.com>
2021-06-19 14:40:46 +0100

    REFACTOR - returns the sum of the numbers it receives separated by commas and '\n'

--- bin/main.dart
+++ bin/main.dart
@@ -1,23 +1,22 @@
 int stringCalculator(String stringNumbers) {
   if (stringNumbers.isEmpty) return 0;
-  if (stringNumbers.contains(',') || stringNumbers.contains('\n')) return _sumNumbersSeparatedByComma(stringNumbers);
-  return stringNumbers.toInt();
+  var numbers = _removeDelimiters(stringNumbers);
+
+  return numbers.sum;
 }
 
-int _sumNumbersSeparatedByComma(String numbers) {
-  var total = 0;
+Iterable<int> _removeDelimiters(String numbersWithDelimiters) {
+  var numbersWithCommas = _transformOtherDelimitersToCommas(numbersWithDelimiters);
 
-  numbers.split(',').forEach((element) {
-    if (element.contains(r'\n')) {
-      element.split(r'\n').forEach((number) => total += number.toInt());
-    } else {
-      total += element.toInt();
-    }
-  });
-
-  return total;
+  return numbersWithCommas.split(',').map((element) => element.toInt());
 }
+
+String _transformOtherDelimitersToCommas(String numbers) => numbers.replaceAll(r'\n', ',');
 
 extension StringExtencion on String {
   int toInt() => int.parse(this);
+}
+
+extension ListIntegersExtension on Iterable<int> {
+  int get sum => reduce((total, number) => total += number);
 }
