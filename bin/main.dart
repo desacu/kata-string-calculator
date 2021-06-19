427d4b351ecb3e0d7b3afd62aa0c0bab18634f65
desacu <noemidsc@gmail.com>
2021-06-19 15:47:57 +0100

    REFACTOR - returns the sum of the numbers it receives separated by specific delimiter

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,48 @@
-int stringCalculator(String stringNumbers) {
-  if (stringNumbers.isEmpty) return 0;
-  if (stringNumbers == '//;' r'\n1;2;3') return 6;
-  var numbers = _removeDelimiters(stringNumbers);
+int stringCalculator(String stringInput) {
+  CalculatorElements? elements;
+
+  if (stringInput.isEmpty) return 0;
+  elements = _separateDelimiterFromNumbers(stringInput);
+  var numbers = _removeDelimiters(elements.stringNumbers, delimiter: elements.delimiter);
 
   return numbers.sum;
 }
 
-Iterable<int> _removeDelimiters(String numbersWithDelimiters) {
-  var numbersWithCommas = _transformOtherDelimitersToCommas(numbersWithDelimiters);
+Iterable<int> _removeDelimiters(String numbersWithDelimiters, {String? delimiter}) {
+  var numbersWithCommas = _transformOtherDelimitersToCommas(numbersWithDelimiters, delimiter: delimiter);
 
   return numbersWithCommas.split(',').map((element) => element.toInt());
 }
 
-String _transformOtherDelimitersToCommas(String numbers) => numbers.replaceAll(r'\n', ',');
+String _transformOtherDelimitersToCommas(String numbers, {String? delimiter}) =>
+    numbers.replaceAll(delimiter ?? r'\n', ',');
+
+CalculatorElements _separateDelimiterFromNumbers(String input) {
+  if (input.isADelimiterCofigured) {
+    var inputElements = input.split(r'\n');
+
+    return CalculatorElements(
+      inputElements.last,
+      inputElements.first.lastChar,
+    );
+  }
+
+  return CalculatorElements(input, null);
+}
+
+class CalculatorElements {
+  String stringNumbers;
+  String? delimiter;
+
+  CalculatorElements(this.stringNumbers, this.delimiter);
+}
 
 extension StringExtencion on String {
   int toInt() => int.parse(this);
+
+  bool get isADelimiterCofigured => startsWith('//');
+
+  String get lastChar => substring(length - 1);
 }
 
 extension ListIntegersExtension on Iterable<int> {
